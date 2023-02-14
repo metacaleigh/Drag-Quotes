0caf6591960e7522a5d3082a18d5612eefdfe1e8
caleigh <caleighsteill@outlook.com>
2023-02-13 17:44:18 -0700

    front end, CSR, queens list, end points

---
+++ my-app-frontend/src/components/QueensHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function QueensHeader() {
+    return(
+    <div className="queens-header-image">
+        <img src="/images/QueensPageHeaderImage.png" alt="Drag Race Alumni"/>
+    </div>
+    )
+}
+
+export default QueensHeader;
